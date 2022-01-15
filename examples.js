18d43b98a12103598633dd553b3bb57d985b701a
nataoleynikova <81632165+nataoleynikova@users.noreply.github.com>
2022-01-15 20:03:13 +0300

    final_v.2

--- examples.js
+++ examples.js
@@ -47,55 +47,60 @@ function change_word() {
 //            if mode = high: sum * 3
 function change_sum() {
   let mode_and_sum = document.getElementById('mode').textContent;
-  let sumn = document.getElementById('sum').textContent;
+  let sum = document.getElementById('sum').textContent;
   if (mode_and_sum === 'low') {
-    document.getElementById('multi').innerHTML = sumn;
+    document.getElementById('multi').innerHTML = sum;
   } else if (mode_and_sum === 'middle') {
-    let sum_2 = Number(sumn) * 2;
+    let sum_2 = Number(sum) * 2;
     document.getElementById('multi').innerHTML = sum_2;
   } else if (mode_and_sum === 'high') {
-    let sum_3 = Number(sumn) * 3;
+    let sum_3 = Number(sum) * 3;
     document.getElementById('multi').innerHTML = sum_3;
   }
 }
 
 // задание 7: вывести четные числа от 0 до 26
 function test_circle() {
+  let given_number_seven = Number(document.getElementById('given_number_seven').value);
   let even_numbers = [];
-  for (let i=0; i<=26; i+=2) {
+  for (let i=0; i<=given_number_seven; i+=2) {
     even_numbers.push(i);
   }
   document.getElementById('circle_id').innerHTML = even_numbers;
 }
 
-// задание 8: массивы: объединение, вывод элемента, получение ключей
+// задание 8.1: массивы: объединение массивов
 function test_array() {
-  let arr1 = [1, 2, 3];
+  let arr1 = [1, 2, 5];
   let arr2 = [4, 5, 6];
-
-  let result = arr1.concat(arr2);
+  let sum_arrs = arr1.concat(arr2);
+  let result = Array.from(new Set(sum_arrs));
   document.getElementById('array').innerHTML = result;
-  document.getElementById('output').innerHTML = result.pop();
-  
-  console.log(Object.keys(result));
-  console.log(Object.keys(result.pop()));
+}
+
+// задание 8.2: массивы: вывод ключей
+function keys_array() {
+  arr = {pet: "собака", name: "Бобик", age: 4, paws: 4, ears: 2, tail: 1};
+  document.getElementById('keys').innerHTML = arr.name + ',' + ' ' + arr.age;
 }
 
 // задание 9: рекурсия: вычислить сумму до данного
 function sum_to() {
-  function sum_to(n) {
-    if (n == 1) return 1;
-    return n + sum_to(n - 1);
+  let given_number_nine = Number(document.getElementById('given_number_nine').value);
+  function sum_to(given_number_nine) {
+    if (given_number_nine == 1) return 1;
+    return given_number_nine + sum_to(given_number_nine - 1);
   }
-  document.getElementById('recursion').innerHTML = sum_to(5);
+  document.getElementById('recursion').innerHTML = sum_to(given_number_nine);
 }
 
 // задание 10: рекурсия: получение чисел Фибоначчи
 function get_numbers() {
+  let given_number_ten = Number(document.getElementById('given_number_ten').value);
   let first = 0;
   let second = 1;
   let numbers = [];
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= given_number_ten; i++) {
     let current = first + second;
     first = second;
     second = current;
